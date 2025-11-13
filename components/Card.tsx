1c684161e09234a02a21d8cd8f45219edb494d20
Dibz.Dev <tommwaduka@gmail.com>
2025-11-13 16:05:25 +0300

    added assets images

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import HOUSE_IMAGE from "@/public/assets/imageshouse.png"
+import HOUSE_IMAGE from "@/public/assets/images/house.png"
 import STAR_IMAGE from "@/public/assets/images/star.png"
 import React from "react"
 import Pill from "./Pill"
